7f2d21fb8bd1d5043491a2be7d0a0c68115d215c
Terry <terrymakonese7@gmail.com>
2026-04-16 20:49:07 +0100

    Fix rate limiter trust proxy for Render

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const routeRoutes = require("./routes/routeRoutes");
 const twilioRoutes = require("./routes/twilioRoutes");
 
 const app = express();
+app.set("trust proxy", 1);
 
 if (!process.env.MONGO_URI) {
   throw new Error("MONGO_URI is required");
