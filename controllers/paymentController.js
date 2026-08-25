82eee96a53b3ca7d84286f85a9ee49c9bcb200da
Terry <terrymakonese7@gmail.com>
2026-08-25 21:58:54 +0100

    Add Google Pay and Apple Pay to Stripe payment methods

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -30,7 +30,7 @@ exports.createCheckoutSession = async (req, res) => {
     }
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "link"],
+      payment_method_types: ["card", "link", "google_pay", "apple_pay"], // ✅ added Google Pay & Apple Pay
       mode: "payment",
       line_items: [
         {
